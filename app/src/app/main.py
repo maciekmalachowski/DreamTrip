af2aaaa2a13ab0d934ba75d3c4694699db2c4573
maciekmalachowski <maciejmalachowski1@gmail.com>
2025-03-21 07:06:58 +0100

    Working app

--- app/src/app/main.py
+++ app/src/app/main.py
@@ -1,66 +1,41 @@
 #!/usr/bin/env python
-import sys
 import warnings
-
 from datetime import datetime
-
-from app.crew import App
+from app.crew import TripAgents
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
-
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
 
 def run():
     """
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'city': 'Gdansk',
+        'date': str(datetime.now().year),
+        'date_range': '3',
+        'budget': '100 euro',
+        'group_type': 'solo traveler'
     }
     
     try:
-        App().crew().kickoff(inputs=inputs)
+        result = TripAgents().crew().kickoff(inputs=inputs)
+        print(result)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
-
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        App().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        App().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
 
 def test():
     """
     Test the crew execution and returns the results.
     """
     inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
+        'city': 'Naples',
+        'date': str(datetime.now().year),
+        'date_range': '7',
+        'budget': '1000 euro',
+        'group_type': 'solo traveler'
     }
     try:
-        App().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+        TripAgents().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
